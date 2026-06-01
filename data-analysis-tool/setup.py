93ed48faa6c46e485fb39307b026b17c04304190
e23254-cyber <e23254@eng.pdn.ac.lk>
2026-06-01 21:33:36 +0530

    Update setup.py

--- data-analysis-tool/setup.py
+++ data-analysis-tool/setup.py
@@ -4,11 +4,5 @@ setup(
     name='data-analysis-tool',
     version='0.1',
     packages=find_packages(),
-    install_requires=[
-        'pandas',
-        'numpy',
-        'plotly',
-        'scipy',
-        'scikit-learn'
-    ]
+    install_requires=[] # Left empty so Colab uses its own built-in versions
 )
